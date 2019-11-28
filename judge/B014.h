8e6487ac392f79c7bb1c621a07e4dcfe16744955
foxzzz <i@foxzzz.com>
2019-11-28 10:58:05 +0800

    update B014

--- judge/B014.h
+++ judge/B014.h
@@ -53,66 +53,66 @@ a.b.c.d
 
 namespace foxzzz {
 
-	class TheJudge : public StrictJudge {
+	class TheJudge : public LooseJudge {
 	public:
-		TheJudge() : StrictJudge(ID, "¼ì²âIPµØÖ·") {}
+		TheJudge() : LooseJudge(ID, "¼ì²âIPµØÖ·") {}
 
 	private:
-		//virtual ProcedureCall verify() {
-		//	return verifyCode;
-		//}
+		virtual ProcedureCall verify() {
+			return verifyCode;
+		}
 
-		//static void verifyCode() {
-		//	int number;
-		//	std::cin >> number;
-		//	for (int index = 0; index < number; ++index) {
-		//		char ip[128] = { 0 };
-		//		std::cin >> ip;
-		//		char* cursor = ip;
-		//		int count = 0;
-		//		bool first = true;
-		//		int part = 0;
-		//		bool succeed = true;
-		//		while (true) {
-		//			if (*cursor >= '0' && *cursor <= '9') {
-		//				if (first) {
-		//					part = *cursor - '0';
-		//					first = false;
-		//				}
-		//				else {
-		//					part *= 10;
-		//					part += *cursor - '0';
-		//				}
-		//			}
-		//			else if (*cursor == '.') {
-		//				++count;
-		//				if (first || part < 0 || part > 255) {
-		//					succeed = false;
-		//					break;
-		//				}
-		//				first = true;
-		//			}
-		//			else if (*cursor == '\0') {
-		//				++count;
-		//				if (count != 4 || first || part < 0 || part > 255) {
-		//					succeed = false;
-		//				}
-		//				break;
-		//			}
-		//			else {
-		//				succeed = false;
-		//				break;
-		//			}
-		//			++cursor;
-		//		}
-		//		if (succeed) {
-		//			std::cout << "Yes" << std::endl;
-		//		}
-		//		else {
-		//			std::cout << "No" << std::endl;
-		//		}
-		//	}
-		//}
+		static void verifyCode() {
+			int number;
+			std::cin >> number;
+			for (int index = 0; index < number; ++index) {
+				char ip[128] = { 0 };
+				std::cin >> ip;
+				char* cursor = ip;
+				int count = 0;
+				bool first = true;
+				int part = 0;
+				bool succeed = true;
+				while (true) {
+					if (*cursor >= '0' && *cursor <= '9') {
+						if (first) {
+							part = *cursor - '0';
+							first = false;
+						}
+						else {
+							part *= 10;
+							part += *cursor - '0';
+						}
+					}
+					else if (*cursor == '.') {
+						++count;
+						if (first || part < 0 || part > 255) {
+							succeed = false;
+							break;
+						}
+						first = true;
+					}
+					else if (*cursor == '\0') {
+						++count;
+						if (count != 4 || first || part < 0 || part > 255) {
+							succeed = false;
+						}
+						break;
+					}
+					else {
+						succeed = false;
+						break;
+					}
+					++cursor;
+				}
+				if (succeed) {
+					std::cout << "Yes" << std::endl;
+				}
+				else {
+					std::cout << "No" << std::endl;
+				}
+			}
+		}
 	};
 
 	LAUNCH_JUDGE(TheJudge)
